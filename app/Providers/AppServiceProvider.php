faee88ccaa69162f357a7246d3dc23c829ae5ece
Caue Prado <caue.prado@i9xp.com.br>
2019-08-20 12:58:20 -0300

    Controller Corredores and Provas created

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+//
     }
 
     /**
@@ -23,8 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-            if($this->app->environment() !== 'production') {
-                $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-            }
+        if($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $this->app->singleton(\Faker\Generator::class, function () {
+                return \Faker\Factory::create('pt_BR');
+            });
+        }
     }
 }
